15dc6855cb6460a8b0867d277eba26a6d1fa96eb
SarahAlAshwal <sa.alashwal@gmail.com>
2020-11-29 22:31:27 -0500

    prepared the schema

--- server/app.js
+++ server/app.js
@@ -1,6 +1,14 @@
 const express = require("express");
+//middleware function that will be used to have the schema as an input
+const {graphqlHTTP} = require("express-graphql"); 
+const schema = require("./schema/schema");
 
 const app = express(); 
+
+app.use('/graphql', graphqlHTTP({
+  schema
+
+}));
 
 app.listen(8000, ()=> {
   console.log("now listening for requests on port 8000");
