499f2b5916804a08a63066ad09d36a528dd88f5e
SarahAlAshwal <sa.alashwal@gmail.com>
2020-12-01 11:27:41 -0500

    adding types to the schema

--- server/app.js
+++ server/app.js
@@ -2,8 +2,16 @@ const express = require("express");
 //middleware function that will be used to have the schema as an input
 const {graphqlHTTP} = require("express-graphql"); 
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+
 
 const app = express(); 
+
+mongoose.connect("mongodb+srv://Sarah:SarahAli@cluster0.bvzwq.mongodb.net/Book List?retryWrites=true&w=majority");
+mongoose.connection.once('open', ()=> {
+  console.log('connected to the database');
+})
+
 
 app.use('/graphql', graphqlHTTP({
   schema
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,14 +1,52 @@
 //The schema file purpose is to define types, their relations and requeries
 const graphql = require('graphql'); 
 
-const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql; 
+const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList} = graphql; 
+const _ = require("lodash");
+
+//dummy data
+let books = [
+  {name: "Name of the wind", genre: 'Fantasy', id: '1', authorId: '1'},
+  {name: "The final Empire", genre: 'Fantasy', id: '2', authorId: '2'},
+  {name: "The long earth", genre: 'Sci-Fi', id: '3', authorId: '3'},
+  {name: "The hero of ages", genre: 'Fantasy', id: '4', authorId: '2'},
+  {name: "The color of magic", genre: 'Fantasy', id: '5', authorId: '3'},
+  {name: "The light fantastice", genre: 'Fantasy', id: '6', authorId: '3'}
+];
+
+let authors = [
+  {name: 'Patrick', age: 44, id : '1'},
+  {name: 'Brandon', age: 42, id : '2'},
+  {name: 'Terry', age: 66, id : '3'},
+];
 
 const BookType = new GraphQLObjectType ({
   name: 'Book',
-  fields: () => ({
-    id: {type: GraphQLString },
+  fields: () => ({ // field is rapped inside a function to avoid AuthorType is not defined. the whole file is executed then what is inside the function
+    id: {type: GraphQLID },
     name: {type: GraphQLString},
-    genre: {type: GraphQLString}
+    genre: {type: GraphQLString},
+    author: {
+      type: AuthorType,
+      resolve(parent, args){
+        return _.find(authors, parent.authorId )
+      }
+    }
+  })
+});
+
+const AuthorType = new GraphQLObjectType ({
+  name: 'Author',
+  fields: () => ({
+    id: {type: GraphQLID },
+    name: {type: GraphQLString},
+    age: {type: GraphQLInt},
+    books: { 
+      type: new GraphQLList(BookType),
+      resolve(parent, args){
+        return _.filter(books, {authorId : parent.id})
+      }
+    }
   })
 });
 
@@ -17,9 +55,30 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     book: {
       type: BookType,
-      args: {id: {type: GraphQLString}},
+      args: {id: {type: GraphQLID}},
       resolve(parent, args) {
         // code to get data from db / other source
+       return _.find(books,{id: args.id});
+      }
+    },
+    author: {
+      type: AuthorType,
+      args: {id: {type: GraphQLID}},
+      resolve(parent, args) {
+        // code to get data from db / other source
+       return _.find(authors,{id: args.id});
+      }
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return books;
+      }
+    },
+    authors: {
+      type: new GraphQLList (AuthorType),
+      resolve(parent, args) {
+        return authors;
       }
     }
   }
