d2b504e09232a4eea9a1411eb074f3421f7bcd9a
SarahAlAshwal <sa.alashwal@gmail.com>
2020-12-02 11:09:13 -0500

    add component BookList

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+//components
+import BookList from "./components/BookList";
 
+//Apollo Client Setup
+const client = new ApolloClient({
+  uri: "http://localhost:8000/graphql"
+})
 
 function App() {
   return (
-    <div id= "main">
-     
-    </div>
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1> My Book List App </h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
+
   );
 }
 
---
+++ client/src/components/BookList.js
@@ -0,0 +1,22 @@
+import {gql} from 'apollo-boost';
+import {graphql} from 'react-apollo';
+
+const getBooksQuery = gql`
+  {
+    books {
+      name,
+      id
+    }
+  }
+`
+function BookList() {
+  return (
+    <div id= "main">
+     <ul id="book-list">
+        <li>Book name</li>
+     </ul>
+    </div>
+  );
+}
+
+export default graphql(getBooksQuery)(BookList);
